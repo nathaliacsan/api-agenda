9a3e64810a556ece2e87af2972ab8683e5b44770
Nathália Santos <nathaliacsan@gmail.com>
2020-11-07 11:26:39 -0300

    Criei o servidor

---
+++ api/src/routes/index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (request, response) => {
+    response.status(200).send({
+        titulo: "Agenda de contatinhos - Reprograma",
+        version: "1.0.0"
+    })
+})
+module.exports = router 
